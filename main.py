956875d90ac14430a400b2ca0e9e70134a53223f
ParthaPratimDatta <partha.datta@adani.com>
2023-05-01 19:54:32 +0530

    Adding py scripts

---
+++ main.py
@@ -0,0 +1,8 @@
+import platform
+import os
+
+print("Hi! This is Partha")
+
+print("\nName of the OS:",os.name)
+print("\nName of the OS system:",platform.system())
+print("\nVersion of the OS:",platform.release())
